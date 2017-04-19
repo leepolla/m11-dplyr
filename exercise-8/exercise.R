a1f54bb7f54349d3e8ff58640daa2334aae79960
leepollagit config --global user.name leepollagit config --global user.name leepolla <leepolla@gmail.com>
2017-04-19 14:19:32 -0700

    quiz section work

--- exercise-8/exercise.R
+++ exercise-8/exercise.R
@@ -6,22 +6,22 @@ pulitzer <- read.csv("data/pulitzer-circulation-data.csv", stringsAsFactors = FA
 # Install and load the needed libraries
 # Be sure to comment out the install.packages function so it won't install it every time it runs
 # Remeber you only need to install a package once
-#install.packages(dplyr)
+install.packages(dplyr)
 library(dplyr)
 
 # View in the data set. Start to understand what the data columns contains
 # Be sure to comment out the function so it won't view everytime you run the code.
-
+View(pulitzer)
 
 # Use 'colnames' to print out the names of the columns
-
+print(colnames(pulitzer))
 
 # Use 'str' to print what types of values are contained in each column
 # Did any value type surprise you? Why do you think they are that type?
-
+str(pulitzer)
 
 # Add a column that contains the diffrence in changes in Daily Circulation from 2004 to 2013
-
+pulitzer <- mutate(pulitzer, change = Daily.Circulation..2013 - Daily.Circulation..2004)
 
 # What publication gained the most pulitzer prizes from 2004-2014?
 # Be sure to use the pipe operator! 
